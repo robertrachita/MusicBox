d357d00f5400f4b9dfb8822c9be1ca8371384cce
Robert Rachita <72192997+robertrachita@users.noreply.github.com>
2021-11-25 13:21:48 +0100

    Database
    >database v1
    >config file for connection

---
+++ db.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE IF NOT EXISTS `MUSICDATE` DEFAULT CHARACTER SET utf8mb4;
+USE `MUSICDATE`;
+
+CREATE TABLE IF NOT EXISTS `users` (
+    `user_id`       INT UNIQUE NOT NULL AUTO_INCREMENT,
+    `email`         VARCHAR(255) UNIQUE NOT NULL,
+    `first_name`    VARCHAR(50) NOT NULL,
+    `last_name`     VARCHAR(50) NOT NULL,
+    `date_of_birth` DATE NOT NULL,
+    `city`          VARCHAR(30) NOT NULL,
+    `country`       VARCHAR(30) NOT NULL,
+    `instrument`    VARCHAR(30) NOT NULL,
+    `password`      VARCHAR(255) NOT NULL,
+
+    INDEX(`email`, `password`),
+    CONSTRAINT PK_user_id PRIMARY KEY (user_id)
+)ENGINE = InnoDB AUTO_INCREMENT = 1001 DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `music_sheets` (
+    `music_sheet_id`    INT UNIQUE NOT NULL AUTO_INCREMENT,
+    `user_id`           INT,
+    `file_path`         VARCHAR(255) NOT NULL,
+
+    CONSTRAINT PK_music_sheet_id PRIMARY KEY (music_sheet_id),
+    CONSTRAINT FK_musicpath_user FOREIGN KEY (user_id)
+                                REFERENCES users (user_id)
+                                ON UPDATE CASCADE
+                                ON DELETE CASCADE
+)ENGINE = InnoDB AUTO_INCREMENT = 1001 DEFAULT CHARSET = utf8mb4;
