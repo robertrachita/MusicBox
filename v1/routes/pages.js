03b33c8882ccbf5510a7d896decaa6b3acc06e29
Atanas <atanas910@gmail.com>
2021-12-01 14:10:21 +0100

    v1.0 Login and Registering working
    Connected frontend to backend
    
    Co-Authored-By: lineeam <72556262+lineeam@users.noreply.github.com>

---
+++ v1/routes/pages.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const authController = require('../controllers/auth');
+
+const router = express.Router();
+
+router.get('/',authController.isLoggedIn, (req,res)=> {
+    res.render('index',{
+        user: req.user
+    });
+});
+
+
+router.get('/register',(req,res)=> {
+    res.render('register');
+});
+
+router.get('/login',(req,res)=> {
+    res.render('login');
+});
+
+router.get('/login2',(req,res)=> {
+    res.render('login2');
+});
+
+router.get('/profile', authController.isLoggedIn, (req,res)=> {
+   
+    if(req.user){
+        res.render('profile', {
+            user: req.user
+        });
+    }
+    else {
+        res.redirect('/login');
+    }
+
+});
+
+module.exports = router;
+
