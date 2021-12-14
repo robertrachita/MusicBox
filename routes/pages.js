05b2812f231619a80668e5eaa4cd06e21d9c69e6
samklop <dagelijkseupload@hotmail.com>
2021-12-14 15:44:30 +0100

    Uploading

--- routes/pages.js
+++ routes/pages.js
@@ -1,30 +1,31 @@
 const express = require('express');
 const authController = require('../controllers/auth');
-
+const path = require('path');
 const router = express.Router();
 
-router.get('/',authController.isLoggedIn, (req,res)=> {
-    res.render('index',{
+const app = express();
+
+router.get('/', authController.isLoggedIn, (req, res) => {
+    res.render('index', {
         user: req.user
     });
 });
 
 
-router.get('/register',(req,res)=> {
+router.get('/register', (req, res) => {
     res.render('register');
 });
 
-router.get('/login',(req,res)=> {
+router.get('/login', (req, res) => {
     res.render('login');
 });
 
-router.get('/login2',(req,res)=> {
+router.get('/login2', (req, res) => {
     res.render('login2');
 });
 
-router.get('/profile', authController.isLoggedIn, (req,res)=> {
-   
-    if(req.user){
+router.get('/profile', authController.isLoggedIn, (req, res) => {
+    if (req.user) {
         res.render('profile', {
             user: req.user
         });
@@ -32,8 +33,17 @@ router.get('/profile', authController.isLoggedIn, (req,res)=> {
     else {
         res.redirect('/login');
     }
-
+    
 });
 
-module.exports = router;
+router.get('/upload', authController.isLoggedIn, (req, res) => {
+    if (req.user) {
+        res.render('upload');
+    } else {
+        res.redirect('/login');
+    }
+});
 
+router.get('/upload', express.static(path.join(__dirname, 'public/upload')));
+
+module.exports = router;
