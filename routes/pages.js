19cba51f5a811d8f69ac9e5cee4930b94d94b2c7
Robert Rachita <72192997+robertrachita@users.noreply.github.com>
2021-12-21 15:00:10 +0100

    Squashed commit of the following:
    commit a25ba4296d4e7902f8631e255f0ad5b8aa8318e4
    Author: Robert Rachita <72192997+robertrachita@users.noreply.github.com>
    Date:   Tue Dec 21 14:57:06 2021 +0100
    
        fixed some more merge errors and bad code

--- routes/pages.js
+++ routes/pages.js
@@ -3,17 +3,13 @@ const authController = require('../controllers/auth');
 const path = require('path');
 const router = express.Router();
 
-
-
 const app = express();
-
 
 router.get('/', authController.isLoggedIn, (req, res) => {
     res.render('index', {
         user: req.user
     });
 });
-
 
 router.get('/register', (req, res) => {
     res.render('register');
@@ -23,20 +19,18 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
-
 router.get('/index', (req, res) => {
     res.render('index');
 });
 
-router.get('/profile', authController.isLoggedIn, (req, res) => {
-
+router.get('/profile', authController.isLoggedIn, (req, res) => {})
 
 router.get('/login2', (req, res) => {
     res.render('login2');
 });
 
 router.get('/profile', authController.isLoggedIn, (req, res) => {
-developing
+    developing
     if (req.user) {
         res.render('profile', {
             user: req.user
@@ -44,7 +38,7 @@ developing
     } else {
         res.redirect('/login');
     }
-    
+
 });
 
 router.get('/upload', authController.isLoggedIn, (req, res) => {
@@ -55,9 +49,6 @@ router.get('/upload', authController.isLoggedIn, (req, res) => {
     }
 });
 
-module.exports = router;
-
 router.get('/upload', express.static(path.join(__dirname, 'public/upload')));
 
 module.exports = router;
-
