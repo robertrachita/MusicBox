d8d1680ac6656839351686aa2de2c332c716a001
Robert Rachita <72192997+robertrachita@users.noreply.github.com>
2021-12-21 04:37:46 -0800

    Release heroku (#9)
    * Database
    
    >database v1
    >config file for connection
    
    * Set up the modules login done
    
    * Registering progress
    
    * Added .gitignore and .editorconfig
    
    Use NPM to get node_modules instead of bundling them in the repository.
    
    * Updated .gitignore and db.sql
    
    Added the .idea folder to .gitignore so JetBrains software doesn't spam the repository.
    
    Changed the indenting of db.sql and fixed it's indexes and foreign keys.
    - Removed index from password
    - Removed duplicate indexes
     - user_id in music_sheets gets set to null if user gets deleted
    
    * LoginRegisterWorking
    
    LOgin + Register + cookies + SESSIONs (I am a god)
    
    * Create .env.example
    
    * Some fileuploading functionality
    
    So I added some basic fileuploading functionality, but it seems to get stuck at req.files undefined. Don't know how to fix this, please help!
    
    * LoginRegistger
    
    Uploading it again
    
    * We need .env
    
    * Please work now
    
    * changed folder structure
    
    >to accomodate for the globe update
    
    * Update style.css
    
    * Uploading
    
    * globe v1
    
    * globe v2
    
    plus css fixes
    
    * Index page added
    
    Index page is added please check it out
    
    Co-Authored-By: lineeam <72556262+lineeam@users.noreply.github.com>
    
    * fixed merge conficlts
    
    * heroku
    
    * Update app.js
    
    Co-authored-by: Atanas <atanas910@gmail.com>
    Co-authored-by: Edward <samklop02@gmail.com>
    Co-authored-by: Lightsbringer <72557734+Lightsbringer@users.noreply.github.com>
    Co-authored-by: levistieber <72553310+levistieber@users.noreply.github.com>
    Co-authored-by: samklop <dagelijkseupload@hotmail.com>
    Co-authored-by: naskodh09 <72555929+naskodh09@users.noreply.github.com>
    Co-authored-by: lineeam <72556262+lineeam@users.noreply.github.com>

--- routes/pages.js
+++ routes/pages.js
@@ -1,39 +1,63 @@
 const express = require('express');
 const authController = require('../controllers/auth');
-
+const path = require('path');
 const router = express.Router();
 
-router.get('/',authController.isLoggedIn, (req,res)=> {
-    res.render('index',{
+
+
+const app = express();
+
+
+router.get('/', authController.isLoggedIn, (req, res) => {
+    res.render('index', {
         user: req.user
     });
 });
 
 
-router.get('/register',(req,res)=> {
+router.get('/register', (req, res) => {
     res.render('register');
 });
 
-router.get('/login',(req,res)=> {
+router.get('/login', (req, res) => {
     res.render('login');
 });
 
-router.get('/login2',(req,res)=> {
+
+router.get('/index', (req, res) => {
+    res.render('index');
+});
+
+router.get('/profile', authController.isLoggedIn, (req, res) => {
+
+
+router.get('/login2', (req, res) => {
     res.render('login2');
 });
 
-router.get('/profile', authController.isLoggedIn, (req,res)=> {
-   
-    if(req.user){
+router.get('/profile', authController.isLoggedIn, (req, res) => {
+developing
+    if (req.user) {
         res.render('profile', {
             user: req.user
         });
-    }
-    else {
+    } else {
         res.redirect('/login');
     }
-
+    
 });
+
+router.get('/upload', authController.isLoggedIn, (req, res) => {
+    if (req.user) {
+        res.render('upload');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+module.exports = router;
+
+router.get('/upload', express.static(path.join(__dirname, 'public/upload')));
 
 module.exports = router;
 
