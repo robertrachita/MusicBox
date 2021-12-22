1db4e8c6beb5c4ec99ac241f7a84161ec7f643d7
Atanas <atanas910@gmail.com>
2021-12-22 12:24:14 +0100

    Fixed once again index page
    This time check it properly, and do not just accept the pull request. I am not fixing this for the 3rd time :)
    
    Co-Authored-By: lineeam <72556262+lineeam@users.noreply.github.com>

--- routes/pages.js
+++ routes/pages.js
@@ -3,10 +3,7 @@ const authController = require('../controllers/auth');
 const path = require('path');
 const router = express.Router();
 
-
-
 const app = express();
-
 
 router.get('/', authController.isLoggedIn, (req, res) => {
     res.render('index', {
@@ -23,25 +20,17 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
-
-router.get('/index', (req, res) => {
-    res.render('index');
-});
-
-router.get('/profile', authController.isLoggedIn, (req, res) => {
-
-
 router.get('/login2', (req, res) => {
     res.render('login2');
 });
 
 router.get('/profile', authController.isLoggedIn, (req, res) => {
-developing
     if (req.user) {
         res.render('profile', {
             user: req.user
         });
-    } else {
+    }
+    else {
         res.redirect('/login');
     }
     
@@ -55,9 +44,15 @@ router.get('/upload', authController.isLoggedIn, (req, res) => {
     }
 });
 
-module.exports = router;
+router.get('/index', authController.isLoggedIn, (req, res) => {
+    if (req.user) {
+        res.render('index');
+    } else {
+        res.redirect('/login');
+    }
+});
+
 
 router.get('/upload', express.static(path.join(__dirname, 'public/upload')));
 
 module.exports = router;
-
