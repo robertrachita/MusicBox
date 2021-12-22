12db71ce56f69f5b3afff2c07bfade81ad4fc6eb
Robert Rachita <72192997+robertrachita@users.noreply.github.com>
2021-12-22 10:20:38 +0100

    Revert "Merge pull request #7 from robertrachita/IndexPage"
    This reverts commit ddbdbed60eb9ac1ff259855376397ad8a22dfd7d, reversing
    changes made to a7a6718fed8623f9d3c58242d6bdd986200fd388.

--- routes/pages.js
+++ routes/pages.js
@@ -3,10 +3,7 @@ const authController = require('../controllers/auth');
 const path = require('path');
 const router = express.Router();
 
-
-
 const app = express();
-
 
 router.get('/', authController.isLoggedIn, (req, res) => {
     res.render('index', {
@@ -23,25 +20,17 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
-
-router.get('/index', (req, res) => {
-    res.render('index');
-});
-
-router.get('/profile', authController.isLoggedIn, (req, res) => {
-
-
 router.get('/login2', (req, res) => {
     res.render('login2');
 });
 
 router.get('/profile', authController.isLoggedIn, (req, res) => {
-developing
     if (req.user) {
         res.render('profile', {
             user: req.user
         });
-    } else {
+    }
+    else {
         res.redirect('/login');
     }
     
@@ -55,9 +44,6 @@ router.get('/upload', authController.isLoggedIn, (req, res) => {
     }
 });
 
-module.exports = router;
-
 router.get('/upload', express.static(path.join(__dirname, 'public/upload')));
 
 module.exports = router;
-
